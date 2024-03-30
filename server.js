26ba867be9cf9ce40eb6207a19d6a6b402bd3fb7
Trevor Johnson <trevor9j@protonmail.com>
2024-03-30 13:12:02 -0700

    created server.js, and html and api routes

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ app.use(express.static('public'))
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
 
 //Look at mini project related to module 11 and check out the file system 
+// The public folder holds files that are all front end files.
+// Routes folder are backend flies
